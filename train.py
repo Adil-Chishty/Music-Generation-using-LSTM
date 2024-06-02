37984bbc3f2e410376ad7837873f8e32095268f1
Adil Chishty <chishty911@gmail.com>
2024-06-02 12:55:39 +0500

    Add files via upload

---
+++ train.py
@@ -0,0 +1,96 @@
+import tensorflow.keras as keras
+import matplotlib.pyplot as plt
+from PreProcessing import generate_training_sequences, SEQUENCE_LENGTH
+
+OUTPUT_UNITS = 38
+NUM_UNITS = [256]
+LOSS = "sparse_categorical_crossentropy"
+LEARNING_RATE = 0.001
+EPOCHS = 90
+BATCH_SIZE = 64
+SAVE_MODEL_PATH = "model.h5"
+
+
+def build_model(output_units, num_units, loss, learning_rate):
+    """Builds and compiles model
+
+    :param output_units (int): Num output units
+    :param num_units (list of int): Num of units in hidden layers
+    :param loss (str): Type of loss function to use
+    :param learning_rate (float): Learning rate to apply
+
+    :return model (tf model): Where the magic happens :D
+    """
+
+    # create the model architecture
+    input = keras.layers.Input(shape=(None, output_units))
+    x = keras.layers.LSTM(num_units[0])(input)
+    x = keras.layers.Dropout(0.2)(x)
+
+    output = keras.layers.Dense(output_units, activation="softmax")(x)
+
+    model = keras.Model(input, output)
+
+    # compile model
+    model.compile(loss=loss,
+                  optimizer=keras.optimizers.Adam(learning_rate=learning_rate),
+                  metrics=["accuracy"])
+
+    model.summary()
+
+    return model
+
+
+def plot_history(history):
+    """Plot training history
+
+    :param history: Training history returned by model.fit
+    """
+    plt.figure(figsize=(12, 4))
+
+    # Plot training & validation loss values
+    plt.subplot(1, 2, 1)
+    plt.plot(history.history['loss'])
+    plt.title('Model Loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train'], loc='upper right')
+
+    # Plot training & validation accuracy values
+    plt.subplot(1, 2, 2)
+    plt.plot(history.history['accuracy'])
+    plt.title('Model Accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['Train'], loc='upper left')
+
+    plt.show()
+
+
+def train(output_units=OUTPUT_UNITS, num_units=NUM_UNITS, loss=LOSS, learning_rate=LEARNING_RATE):
+    """Train and save TF model.
+
+    :param output_units (int): Num output units
+    :param num_units (list of int): Num of units in hidden layers
+    :param loss (str): Type of loss function to use
+    :param learning_rate (float): Learning rate to apply
+    """
+
+    # generate the training sequences
+    inputs, targets = generate_training_sequences(SEQUENCE_LENGTH)
+
+    # build the network
+    model = build_model(output_units, num_units, loss, learning_rate)
+
+    # train the model
+    history = model.fit(inputs, targets, epochs=EPOCHS, batch_size=BATCH_SIZE)
+
+    # save the model
+    model.save(SAVE_MODEL_PATH)
+
+    # plot training history
+    plot_history(history)
+
+
+if __name__ == "__main__":
+    train()
